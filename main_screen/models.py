31a70433fa79ded44d080b5399fe22fc72cccfcc
Kamelleon <kamilbugla22@icloud.com>
2022-06-01 22:37:10 +0200

    Created model "Camera"

--- main_screen/models.py
+++ main_screen/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
+CODECS =(
+    ("1", "h264"),
+    ("2", "h265"),
+)
 # Create your models here.
+class Camera(models.Model):
+    name = models.CharField(max_length=30)
+    ip = models.GenericIPAddressField(protocol="IPv4")
+    codec = models.CharField(max_length=1, choices=CODECS)
+    user = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    substream = models.BooleanField(default=False)
