5cfea2de454ad95b02052fe48e70bb5ac9f5cf0a
Kamelleon <kamilbugla22@icloud.com>
2022-06-04 14:24:43 +0200

    Styled registration page

--- register/forms.py
+++ register/forms.py
@@ -1,9 +1,28 @@
+from django.contrib.auth import password_validation
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 
 class CreateUserForm(UserCreationForm):
+    password1 = forms.CharField(
+        label="Password",
+        strip=False,
+        widget=forms.PasswordInput,
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+    password2 = forms.CharField(
+        label="Password confirmation",
+        strip=False,
+        widget=forms.PasswordInput,
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'input-text'
     class Meta:
         model = User
-        fields = ["username","email","password1","password2"]
+        fields = ["username","email"]
+
