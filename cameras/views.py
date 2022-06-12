f4391b61ab14bef8d8f2ca94246abe8731eabf3c
Kamelleon <kamilbugla22@icloud.com>
2022-06-12 21:40:32 +0200

    Changed the way that camera capturing works.

--- cameras/views.py
+++ cameras/views.py
@@ -1,3 +1,5 @@
+import time
+
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .forms import CameraForm
@@ -5,6 +7,7 @@ from .models import Camera
 from django.views.decorators import gzip
 from django.http import StreamingHttpResponse
 import cv2, queue, threading
+import rtsp
 
 
 class VideoCamera:
@@ -15,29 +18,39 @@ class VideoCamera:
         t.daemon = True
         t.start()
 
-    def __del__(self):
-        cv2.destroyAllWindows()
+    # def __del__(self):
+    #     print("deleting")
+    #     self.cap.release()
+    #     # cv2.destroyAllWindows()
+    #
+    # def _reader(self):
+    #     while True:
+    #         ret, frame = self.cap.read()
+    #         if not ret:
+    #             print("No ret, releasing...")
+    #             self.cap.release()
+    #             break
+    #         if not self.q.empty():
+    #             try:
+    #                 self.q.get_nowait()
+    #             except queue.Empty:
+    #                 pass
+    #         self.q.put(frame)
+    #
+    # def read(self):
+    #     return self.q.get()
 
-    def _reader(self):
-        while True:
-            ret, frame = self.cap.read()
-            if not ret:
-                break
-            if not self.q.empty():
-                try:
-                    self.q.get_nowait()
-                except queue.Empty:
-                    pass
-            self.q.put(frame)
-
-    def read(self):
-        return self.q.get()
-
-
-def gen(camera):
-    while True:
-        frame = camera.read()
+    def preprocess(self, frame):
         ret, frame = cv2.imencode('.jpg', frame)
+        return frame
+
+
+def gen():
+    client = rtsp.Client(rtsp_server_uri = 'rtsp://192.168.0.103:8080/video/h264')
+    time.sleep(0.2)
+    while client.isOpened():
+        _image = client.read(raw=True)
+        ret, frame = cv2.imencode('.jpg', _image)
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame.tobytes() + b'\r\n\r\n')
 
@@ -45,7 +58,8 @@ def gen(camera):
 @login_required(login_url='login')
 @gzip.gzip_page
 def rtsp_stream(request, rtsp_ip, port, suffix):
-    return StreamingHttpResponse(gen(VideoCamera(rtsp_ip, port, suffix)),
+    print("Getting stream")
+    return StreamingHttpResponse(gen(),
                                  content_type='multipart/x-mixed-replace; boundary=frame')
 
 
--- detector/detector_system/gst_launcher.py
+++ detector/detector_system/gst_launcher.py
@@ -12,12 +12,12 @@ for camera_object in all_cameras:
     name = camera_object['name']
     user = camera_object['user']
     password = camera_object['password']
+    codec = camera_object['codec']
     suffix = camera_object['suffix']
-    print(ip,port,name,user,password, suffix)
+    print(ip,port,name,user,password,codec, suffix)
 
 
 
-def create_gst_script(ip,port,name,codec):
-    pass
-    # if user == "":
-        # gst_launch_command =
+# def create_gst_script(ip,port,name,codec):
+#     if user == "":
+#         gst_launch_command = 'gst-launch-1.0 rtspsrc location="rtsp://192.168.0.103:8080/video/h264" ! rtph264depay ! avdec_h264 ! videorate ! video/x-raw,framerate=2/1 ! jpegenc ! multifilesink location=kamera.jpg'
